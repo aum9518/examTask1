9c5437b6b6f10f76fd2d17c9b4f0c42b1371e6e4
aum9518 <abduganiuulumanasbek@gmail.com>
2023-01-30 17:05:24 +0600

    add mini calculator

--- src/miniCalc.java
+++ src/miniCalc.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class miniCalc {
+import java.util.Scanner;
+
+public class miniCalc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        char b = in.next().charAt(0);
+        int c = in.nextInt();
+        System.out.println(calc(a,b,c));
+    }
+    static int calc (int a, int b, int c){
+        Scanner in = new Scanner(System.in);
+
+        switch (b){
+            case '+':
+                a=a+c;
+                break;
+            case '-':
+                a=a-c;
+                break;
+            case '*':
+                a=a*c;
+                break;
+            case '/':
+                a=a/c;
+                break;
+            default:
+                System.out.println("Error");
+        }
+       return a;
+    }
+
 }

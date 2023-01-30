b7f07b62a9deb109818310933e94375518701cda
aum9518 <abduganiuulumanasbek@gmail.com>
2023-01-30 17:05:24 +0600

    add mini calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        int [] mas = new int[random.nextInt(10,15)];
+        System.out.println(mas.length+" is length");
+        System.out.println(Arrays.toString(mas(mas)));
+
+    }
+    static int [] mas (int num[]){
+        for (int i = 0; i< num.length;i++){
+            num[i]= i*9;
+        }
+        return num;
     }
 }
